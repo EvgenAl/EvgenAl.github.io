56667e05afc10a6d1cfdd5fc5bd59b5af2aea0a2
Spinto <evgenii_krishtopa@mail.ru>
2016-09-25 22:23:49 +0300

    new Project

--- js/script.js
+++ js/script.js
@@ -1,88 +1,26 @@
-  $( function() {
+$(function(){
+	$('.bxslider').bxSlider({
+		infiniteLoop: false,
+		hideControlOnEnd: true,
+		startSlide:1
+	});
 
+	var answers = $('.faq .answer'),
+	duration = 1000,
+	questions = $('.faq .question');
 
-  	//Select
-  	$('.types').selectmenu();
+	$('.faq .question').click(function(){
+		var answer = $(this).next('.answer');
+		if(answer.is(':hidden')){
+			answers.fadeOut(duration);
+			answer.slideDown(1000);
+			questions.children('.arrow').slideDown(1000);
+			$(this).children('.arrow').fadeOut(1000);
 
-
-  	//Range-clider
-  	$("#slider-range").slider({
-  		range: true,
-  		min: 0,
-  		max: 450,
-  		values: [ 100, 350 ],
-  		slide: function( event, ui) {
-  			$("#amount").val("$" + ui.values[0] + " - $" + ui.values[1]);
-  		}
-  	});
-  	$("#amount").val("$" + $("#slider-range").slider("values", 0) + "k" +
-  		" - $" + $("#slider-range").slider("values", 1) + "k");
-
-
-
-// BX slider
-
-$('.bxslider').bxSlider({
-	mode: 'vertical',
-	slideMargin: 60
-});
-
-
-
-$('.slider4').bxSlider({
-	slideWidth: 120,
-	minSlides: 2,
-	maxSlides: 5,
-	moveSlides: 3,
-	slideMargin: 10
-});
-
-
-
-//Active
-var all = $('.nav a');
-$('.nav a').click(function(){
-	all.removeClass('active');
-	$(this).addClass('active');
-});
-
-
-
-//Scroll
-
-$(".scrollTop, .scrollBottom").mPageScroll2id({
-	scrollSpeed : 500
-});
-var topOffset = $('#vehichles').offset().top;
-var bottomOffset = $('#map').offset().top;
-$('.scroll').hide();
-$(document).scroll(function(){
-	if($(this).scrollTop() >= bottomOffset){
-		$('.scrollBottom').hide();
-	}
-	else{
-		$('.scrollBottom').show();
-	}
-	if($(this).scrollTop() <= topOffset){
-		$('.scrollTop').hide()
-	}
-	else{
-		$('.scrollTop').show();
-	}
-});
-
-
-//Menu
-
-$('.button').click(function(){
-	$('.second_menu ul').slideToggle(500);
-});
-$(window).resize(function() {	
-	//если ширина окна больше ук размера	
-	if (  $(window).width() > 768 ) {
-		//удаляем атрибут стиля	у блока, который был присвоен методом		slideToggle()
-		$('.second_menu ul').removeAttr('style');
-	}
-	});//end resize
-
+		}
+		else{
+			answer.fadeOut(1000);
+			$(this).children('.arrow').slideDown(1000);
+		}
+	});
 });
