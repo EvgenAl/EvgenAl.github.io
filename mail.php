c6e0bf88fdb38746e4225ec45f7a81a45d6ca7bf
Spinto <evgenii_krishtopa@mail.ru>
2016-10-30 20:38:56 +0200

    фф

---
+++ mail.php
@@ -0,0 +1,14 @@
+<?php
+
+$recepient = "evgenii_krishtopa@mail.ru";
+
+
+$name = trim($_POST["name"]);
+$phone = trim($_POST["phone"]);
+$text = trim($_POST["text"]);
+$message = "Имя: $name \nТелефон: $phone \nТекст: $text";
+
+$pagetitle = "Новая заявка";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
+
+?>
