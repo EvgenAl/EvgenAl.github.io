b575b0f71b1a428fe93f72b2291bfed7a0c012da
Spinto <evgenii_krishtopa@mail.ru>
2017-05-25 21:26:01 +0300

    add stilist

--- Agam_prodaction/mail_1.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$recepient = "evgenii_krishtopa@mail.ru";
-
-$email = trim($_POST["email"]);
-$message = "Почта: $email";
-
-$pagetitle = "Новая заявка";
-mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
--- bitcoin/mail_form.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$recepient = "";
-
-$email = trim($_POST["email"]);
-$message = "Почта: $email";
-
-$pagetitle = "Новая заявка";
-mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
