55eedc6bae2dbfd7e83f6c968cc317c24f9200d1
Spinto <evgenii_krishtopa@mail.ru>
2018-04-03 16:13:40 +0300

    delete old projects

--- Micro_Blanding/mail_1.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$recepient = "evgenii_krishtopa@mail.ru";
-
-$phone = trim($_POST["phone"]);
-$message = "Телефон: $email";
-
-$pagetitle = "Новая заявка";
-mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
